6188a5b04cb61581dfde96c55651c2b7479b30dc
evolgenomics <frankyc@tue.mpg.de>
2021-03-29 10:55:36 +0200

    Update 10X_spoof.pl

--- 10X_spoof.pl
+++ 10X_spoof.pl
@@ -19,43 +19,53 @@ my $bx = "";
 my $line = 3;
 my $c= "";
 my $lane = "";
+my $bc_flag = 0;
+my $nbc_flag = 0;
 
 if ($verbose ne "") {
 	#Dumps out the first 12 lines for checking
-	open (R1, "zcat $fastq | head -12 | ");
-	while (<R1>) {
+	open (R1, "zcat $fastq |");
+	VERBOSE: while (<R1>) {
    	     $line++;
    	     #e.g., @ST-J00101:159:HF5TFBBXY:4:1101:30249:1156 BX:Z:A33C26B95D10
    	    ($lane, $bx, $c, $line) = ($1, $2, $3, 0) if (/^@.+\:\d+:\S+:(\d):\d+:\d+:\d+\s+BX:Z:(A..(C..)B..D..)/ && $line==4);
    	     
 			if (exists($barcodes{$bx})) {
+			   if ($bc_flag < 3) {
    	            
-			   if ($line == 0 || $line == 2) {
-						print "\n[BARCODE MATCHED FROM LIST: $bx -> $barcodes{$bx}]\n" if ($line == 0);
-   	                    print;
-   	            } elsif ($line == 1) {
-   	                    my $string;
-   	                    $string .= $chars[rand @chars] for 1..7;
-   	                    print "$barcodes{$bx}".$string.$_;
-   	            } elsif ($line == 3) {
-   	                    print "".("J" x 23)."$_";
-   	                    $bx = "";
-   	            }
+				   if ($line == 0 || $line == 2) {
+								print "\n[BARCODE MATCHED FROM LIST: $bx -> $barcodes{$bx}]\n" if ($line == 0);
+   	       	            		 print;
+   	    		        } elsif ($line == 1) {
+   	    	                my $string;
+   	   	                 $string .= $chars[rand @chars] for 1..7;
+   	   	                 print "$barcodes{$bx}".$string.$_;
+   	   	         } elsif ($line == 3) {
+   	   	                 print "".("J" x 23)."$_";
+   	   	                 $bx = "";
+						 $bc_flag++;	
+   	 	           }
+			   }
    	    } else {
-   	             if ($line == 0 || $line == 2) {
-						print "\n[BARCODE NOT IN LIST: $bx]\n" if ($line == 0);
-   	                     print;
-   	             } elsif ($line == 1) {
-   	                     my $string;
-   	                     print "".("N" x 23).$_;
-   	             } elsif ($line == 3) {
-   	                     print "".("#" x 23).$_;
-	                }
+				if ($nbc_flag == 0) {
+   	       	      if ($line == 0 || $line == 2) {
+							print "\n[BARCODE NOT IN LIST: $bx]\n" if ($line == 0);
+   	       	              print;
+   	       	      } elsif ($line == 1) {
+   	       	              my $string;
+   	      	               print "".("N" x 23).$_;
+   	      	       } elsif ($line == 3) {
+   	       	              print "".("#" x 23).$_;
+						  $nbc_flag++;
+	    	       }
+				}
 	       }
+	   last VERBOSE if ($bc_flag + $nbc_flag == 4);
 	   }
 	close (R1);
 
 	$line = 3;
+	print "NOW PROCESSING THE ENTIRE FASTQ FILE...\n";
 }
 
 open (OUT, " | gzip -c > $fastq_out");
